97483c2b39739e63a505016fb1e61e305d87aa17
Duong Trung Kien <96287101+codingwithkien@users.noreply.github.com>
2023-02-25 22:45:15 +0700

    Create 1_TwoSum.py

---
+++ 1_TwoSum.py
@@ -0,0 +1,25 @@
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+      
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dictionary = {}
+        for index, num in enumerate(nums):
+            if target - num in dictionary:
+                return [dictionary[target - num], index]
+            dictionary[num] = index
+        return []
+      
+ # Coding With Kien
